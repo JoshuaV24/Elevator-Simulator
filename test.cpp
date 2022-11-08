7e443e6f84aab59390eecfd3294fb61c9bda4f1d
JoshuaV24 <111840972+JoshuaV24@users.noreply.github.com>
2022-11-08 18:16:35 -0500

    added tests for elevator

--- test.cpp
+++ test.cpp
@@ -26,14 +26,80 @@
 #include "SatisfactionIndex.h"
 #include "Utility.h"
 
+void elevator_tests();
+
 using namespace std;
 
 // declare your test functions here
 
 void start_tests() {
-    // call your test functions here
+    elevator_tests();
     
     return;
 }
 
 // write test functions here
+void elevator_tests() {
+
+    //testing tick
+    Elevator elevator1;
+    elevator1.setCurrentFloor(4);
+    elevator1.serviceRequest(1);
+    int currentTime1 = 1;
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator1.tick(currentTime1);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    
+
+
+    int currentTime2 = 0;
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator1.tick(currentTime2);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+
+    double currentTime3 = 2.5;
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator1.tick(currentTime3);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    
+    Elevator elevator2;
+    elevator2.setCurrentFloor(3);
+    elevator2.serviceRequest(3);
+    int currentTime4 = 2;
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator2.tick(currentTime4);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+
+    elevator2.setCurrentFloor(0);
+    elevator2.serviceRequest(3);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator2.tick(currentTime4);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+
+    elevator2.setCurrentFloor(5);
+    elevator2.serviceRequest(0);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator2.tick(currentTime4);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+
+    int currentTime5 = 2;
+    elevator2.setCurrentFloor(0);
+    elevator2.serviceRequest(0);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+    elevator2.tick(currentTime5);
+    cout << elevator1.getCurrentFloor() << endl;
+    cout << elevator1.getTargetFloor() << endl;
+}
