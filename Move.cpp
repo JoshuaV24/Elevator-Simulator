c41f9abdfb92885021dee40b75d86b4a8501c5ab
Elisabeth Greiner <113308097+emgblue@users.noreply.github.com>
2022-11-15 19:41:55 -0500

    Update Move.cpp

--- Move.cpp
+++ Move.cpp
@@ -92,7 +92,6 @@ void Move::setPeopleToPickup(const string& pickupList, const int currentFloor, c
     // calculating targetFloor
     for (int j = 0; j < (pickupList.length() -  1); j++) {
         Move target[MAX_PEOPLE_PER_FLOOR] = { };
-        int tempTarget = 0;
         if (abs(target[j].getTargetFloor() - currentFloor) > abs(target[j + 1].getTargetFloor() - currentFloor)) {
             setTargetFloor(target[j].getTargetFloor());
         }
