a8ac2d8aa8a4823115ebcf7c1631671bf7aa8745
JoshuaV24 <111840972+JoshuaV24@users.noreply.github.com>
2022-12-05 19:09:31 -0500

    Added the goUpOrDown function
    It will return "up" if elevator should go up from target floor, and "down" if elevator should go down from target floor.

--- AI.cpp
+++ AI.cpp
@@ -24,3 +24,32 @@ string getAIPickupList(const Move& move, const BuildingState& buildingState,
                        const Floor& floorToPickup) {
     return "";
 }
+
+
+//returns "up" if elevator should go up
+//returns "down" if elevator should go down
+string goUpOrDown(Floor& floor) {
+    //we get number of people on the floor in question
+    int numPeopleOnFloor = floor.getNumPeople();
+    //by the end this should be a positive or negative value
+    int sum = 0;
+
+    for (int i = 0; i < numPeopleOnFloor; i++) {
+        //we get the person at each index
+        Person person = floor.getPersonByIndex(i);
+        //points will be positive if they are going up and negative if going down
+        int points = person.getTargetFloor() - person.getCurrentFloor();
+        //add it to sum
+        sum += points;
+    }
+    //if sum is greater than 0 there are more points to be earned by going up
+    //so we return an up command
+    if (sum >= 0) {
+        return "up";
+    }
+    //if sum is less than 0 there are more points to be earned by going down
+    //so we return a down command
+    else {
+        return "down";
+    }
+}
